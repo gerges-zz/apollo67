7f16b842f524a5629f20b3ddfc7cc78527bed8cb
Issac Gerges <gerges@gerges.local>
2011-11-19 12:37:58 -0600

    Pref screen, trigger text specifiable

--- src/com/sector67/space/MainActivity.java
+++ src/com/sector67/space/MainActivity.java
@@ -1,15 +1,55 @@
 package com.sector67.space;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	private SharedPreferences sharedPreferences;
+	private EditText triggerText; 
+	private Button savePrefs;
+	private TextView currentTrigger;
+	private Button.OnClickListener saveOnClickListener = new Button.OnClickListener(){
+		  @Override
+		  public void onClick(View arg0) {
+		   savePref("trigger", triggerText.getText().toString());
+	       updateDisplay("trigger", "Apollo67 Launch", currentTrigger);
+		  }
+     };
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
+        
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        
+        triggerText = (EditText)findViewById(R.id.triggerText);
+        savePrefs = (Button)findViewById(R.id.savePrefs);
+        savePrefs.setOnClickListener(saveOnClickListener);
+        currentTrigger = (TextView)findViewById(R.id.currentTrigger);
+        
+        updateDisplay("trigger", "Apollo67 Launch", currentTrigger);
+
     }
+    
+    private void updateDisplay (String prefName, String defaultText, TextView tv){
+    	String prefText = sharedPreferences.getString(prefName, defaultText);
+    	tv.setText(prefText);
+    }
+    
+    private void savePref(String key, String value){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
+      
 }
--- src/com/sector67/space/SMSReceiver.java
+++ src/com/sector67/space/SMSReceiver.java
@@ -3,16 +3,22 @@ package com.sector67.space;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.telephony.SmsMessage;
 import android.util.Log;
 
 public class SMSReceiver extends BroadcastReceiver {
-
+	private SharedPreferences sharedPreferences;
     public static final String SMS_EXTRA_NAME = "pdus";
-    public static final String SMS_TRIGGER_TEXT = "Apollo67 Launch";
 
     public void onReceive(Context context, Intent intent ) {
+        if(null == sharedPreferences) {
+        	sharedPreferences =  PreferenceManager.getDefaultSharedPreferences(context);
+        }
+        String triggerText = sharedPreferences.getString("trigger", "Apollo67 Launch");
+
 
         Bundle smsExtras = intent.getExtras();
 
@@ -27,12 +33,12 @@ public class SMSReceiver extends BroadcastReceiver {
                 String body = sms.getMessageBody().toString();
                 String address = sms.getOriginatingAddress();
                 
-                if(body.equalsIgnoreCase(SMS_TRIGGER_TEXT)){
+                if(body.equalsIgnoreCase(triggerText)){
                 	Log.i(SMSReceiver.class.getName(), "SMS recieved from " + address + ", commencing launch");
-                    final Intent spaceIntent = new Intent(context, SpaceActivity.class);
-                    spaceIntent.addFlags(Intent.FLAG_FROM_BACKGROUND); 
-                    spaceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); 
-                    context.startActivity(spaceIntent);
+                    final Intent launchIntent = new Intent(context, LaunchActivity.class);
+                    launchIntent.addFlags(Intent.FLAG_FROM_BACKGROUND); 
+                    launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); 
+                    context.startActivity(launchIntent);
                 }
             }
 
