94a7ac96eb8f4143919f8a955488e6162286fda0
Issac Gerges <gerges@gerges.local>
2011-11-20 01:45:59 -0600

    Add location tracking to launch activity

--- src/com/sector67/space/LaunchActivity.java
+++ src/com/sector67/space/LaunchActivity.java
@@ -9,14 +9,15 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.Window;
 
 import com.sector67.space.service.CamcorderReciever;
+import com.sector67.space.service.LocationService;
 
 
 public class LaunchActivity extends Activity {
-    private PendingIntent mCamcorderSender;
-    private static final long TIME_TO_RECORD = 300000;
+    private PendingIntent mLocationAlarmSender;
 
 	public LaunchActivity() {
 
@@ -29,7 +30,13 @@ public class LaunchActivity extends Activity {
         
         Intent camcorderIntent = new Intent(getBaseContext(), CamcorderReciever.class);
         camcorderIntent.putExtra("timeToRecord", 300*1000);
-
+        
+		mLocationAlarmSender = PendingIntent.getService(LaunchActivity.this,
+                0, new Intent(LaunchActivity.this, LocationService.class), 0);
+		
+        long firstTime = SystemClock.elapsedRealtime();
+		AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE);
+        am.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, firstTime, 60*1000, mLocationAlarmSender);
 		MediaPlayer mPlayer = MediaPlayer.create(this, R.raw.launch_countdown);
 		mPlayer.start();
 		
@@ -43,7 +50,7 @@ public class LaunchActivity extends Activity {
         	   startActivity(spaceIntent);
         	   finish();
             }
-         }, TIME_TO_RECORD);
+         }, 300*1000);
 
     }
 
@@ -54,6 +61,6 @@ public class LaunchActivity extends Activity {
 	public void onDestroy() {
 		super.onDestroy();
 		AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE);
-		am.cancel(mCamcorderSender);
+		am.cancel(mLocationAlarmSender);
 	}
 }
